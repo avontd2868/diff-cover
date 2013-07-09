be6a1d4264274e9f81272e1f9f57f86927be551f
ihoover <ihoover@edx.org>
2013-07-09 12:23:44 -0400

    reorder arguments in bash script

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-nosetests --with-coverage --cover-package=diff_cover
 # git remote update is needed for travis to properly find HEAD
 git remote update
+nosetests --with-coverage --cover-package=diff_cover
 # Create the xml document from the coverage report
 coverage xml
 # Run diff-cover against itself
