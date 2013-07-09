615ac86ff1cbf9a2221f8f48ace70b1458d7f1ce
Ian Hoover <ihoover@edx.org>
2013-07-09 14:47:15 -0400

    Allow for multiple XML inputs. Travis installs and runs diff-cover

---
+++ run_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# git remote update is needed for travis to properly find HEAD
+git remote update
+# Create the xml document from the coverage report
+coverage xml
+# Run diff-cover against itself
+diff-cover coverage.xml
