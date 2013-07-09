584531b6e8805cc7ad77d7a2f03b11225fdfcc79
ihoover <ihoover@edx.org>
2013-07-09 13:04:11 -0400

    monkying to get travis to find the origin/master take 5

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
-#nosetests --with-coverage --cover-package=diff_cover
+nosetests
+nosetests --with-coverage --cover-package=diff_cover
 # git remote update is needed for travis to properly find HEAD
-#git remote update
+git remote update
 # Create the xml document from the coverage report
-#coverage xml
+coverage xml
 # Run diff-cover against itself
-#diff-cover coverage.xml
+diff-cover coverage.xml
