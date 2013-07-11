ee5968387f1b8f42e84918263f86a9d8f8711bf5
Will Daly <will.e.daly@gmail.com>
2013-07-10 22:51:57 -0400

    Added print statement explaining the installation test.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,4 +5,5 @@ nosetests --with-coverage --cover-package=diff_cover
 # We run the install script to verify that it doesn't
 # raise any exceptions -- if it fails
 # then the build should fail in Travis
+echo "Testing installation script..."
 python setup.py install
